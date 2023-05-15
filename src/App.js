1d5f1d2f79797a4f66f13281c4dc51d007dacf0e
atulaksingh <41638504+atulaksingh@users.noreply.github.com>
2023-05-15 20:23:41 +0530

    updated route with exact

--- src/App.js
+++ src/App.js
@@ -17,14 +17,14 @@ function App() {
       <BrowserRouter>
       
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/pages/gridview" element={<GridView />} />
-          <Route path="/pages/columns" element={<Columns />} />
-          <Route path="/shop/products" element={<Product />} />
-          <Route path="/shop/cart" element={<Cart />} />
-          <Route path="/shop/checkout" element={<Checkout />} />
-          <Route path="/shop/my-account" element={<MyAccount />} />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/about" element={<AboutPage />} />
+          <Route exact path="/pages/gridview" element={<GridView />} />
+          <Route exact path="/pages/columns" element={<Columns />} />
+          <Route exact path="/shop/products" element={<Product />} />
+          <Route exact path="/shop/cart" element={<Cart />} />
+          <Route exact path="/shop/checkout" element={<Checkout />} />
+          <Route exact path="/shop/my-account" element={<MyAccount />} />
         </Routes>
       </BrowserRouter>
     </div>
