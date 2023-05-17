cc463c367cec99848b17155a7d2526d6c44aa261
atulaksingh <talktoatulsingh@gmail.com>
2023-05-17 15:13:18 +0530

    update

--- src/App.js
+++ src/App.js
@@ -10,29 +10,14 @@ import Product from "./pages/Product";
 import Cart from "./pages/Cart";
 import Checkout from "./pages/Checkout";
 import MyAccount from "./pages/MyAccount";
-<<<<<<< HEAD
+
 import GridPostPage from "./pages/GridPostPage";
-=======
 import ScrollToTop from "./components/Shared/ScrollToTop";
->>>>>>> 71c988ce4a3249eee0c0271f8abdecec1626dd93
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-<<<<<<< HEAD
-        
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/pages/gridview" element={<GridView />} />
-          <Route path="/pages/columns" element={<Columns />} />
-          <Route path="/shop/products" element={<Product />} />
-          <Route path="/shop/cart" element={<Cart />} />
-          <Route path="/shop/checkout" element={<Checkout />} />
-          <Route path="/shop/my-account" element={<MyAccount />} />
-          <Route path="/gridviewpost" element={<GridPostPage />} />
-=======
         <ScrollToTop />
         <Routes>
           <Route exact path="/" element={<Home />} />
@@ -43,7 +28,6 @@ function App() {
           <Route exact path="/shop/cart" element={<Cart />} />
           <Route exact path="/shop/checkout" element={<Checkout />} />
           <Route exact path="/shop/my-account" element={<MyAccount />} />
->>>>>>> 71c988ce4a3249eee0c0271f8abdecec1626dd93
         </Routes>
       </BrowserRouter>
     </div>
